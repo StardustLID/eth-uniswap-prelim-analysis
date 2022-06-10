c80149ca28d509397a616ff7e5d3f8fd4e05a6fa
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-06-10 17:56:16 +0800

    Basic and async query, histogram, FFT (#1)
    * raw graphql requests
    
    * gql wrapped queries
    
    * output
    
    * top 10 pools
    
    * per pool 24H volume, TVL
    
    * histogram, FFT

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # eth-uniswap-prelim-analysis
 Preliminary data analysis on Uniswap V3 on Ethereum.
+
+```
+$ pip install requests gql[all]
+```
---
+++ graphql_helper.py
@@ -0,0 +1,13 @@
+import requests
+
+# function to use requests.post to make an API call to the subgraph url
+def run_query(url: str, query):
+
+    # endpoint where you are making the request
+    request = requests.post(url, \
+        '', \
+        json={'query': query})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception('Query failed. Return code is {}.      {}'.format(request.status_code, query))
