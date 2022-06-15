b889f9c0ceff50016eabf836d992a8ae04c6e6f4
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-06-15 12:58:29 +0100

    reformat py

--- pyammanalysis/graphql_helper.py
+++ pyammanalysis/graphql_helper.py
@@ -3,10 +3,12 @@ import requests
 # function to use requests.post to make an API call to the subgraph url
 def run_query(url: str, query):
     # endpoint where you are making the request
-    request = requests.post(url, \
-        '', \
-        json={'query': query})
+    request = requests.post(url, "", json={"query": query})
     if request.status_code == 200:
         return request.json()
     else:
-        raise Exception('Query failed. Return code is {}.      {}'.format(request.status_code, query))
+        raise Exception(
+            "Query failed. Return code is {}.      {}".format(
+                request.status_code, query
+            )
+        )
--- pyammanalysis/pwlf_helper.py
+++ pyammanalysis/pwlf_helper.py
@@ -3,9 +3,10 @@ from typing import Any
 import numpy as np
 import pwlf
 
+
 @dataclass
 class PwlfResult:
-    tp: np.ndarray # turning points
+    tp: np.ndarray  # turning points
     slopes: np.ndarray
     xHat: np.ndarray
     yHat: np.ndarray
@@ -13,13 +14,16 @@ class PwlfResult:
     se: np.ndarray
     predict: Any
 
-def regression(x: np.ndarray, y: np.ndarray, n_segments: int, step_size: float = 0.0001) -> PwlfResult:
+
+def regression(
+    x: np.ndarray, y: np.ndarray, n_segments: int, step_size: float = 0.0001
+) -> PwlfResult:
     # fit your data (x and y)
     # pwlf uses np internally, but accepts either np.ndarray or pd.core.series.Series
     myPWLF = pwlf.PiecewiseLinFit(x, y)
 
     # fit the data for n line segments
-    z = myPWLF.fit(n_segments) # returns np.ndarray of np.float64
+    z = myPWLF.fit(n_segments)  # returns np.ndarray of np.float64
 
     # calculate slopes
     slopes = myPWLF.calc_slopes()
@@ -29,7 +33,7 @@ def regression(x: np.ndarray, y: np.ndarray, n_segments: int, step_size: float =
     yHat = myPWLF.predict(xHat)
 
     # calculate statistics
-    p = myPWLF.p_values(method='non-linear', step_size=step_size) # p-values
+    p = myPWLF.p_values(method="non-linear", step_size=step_size)  # p-values
     se = myPWLF.se  # standard errors
 
     return PwlfResult(z, slopes, xHat, yHat, p, se, myPWLF.predict)
