7158fc109c993673a43844c384822921181e6562
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-06-15 14:50:36 +0100

    add flake8 for python style check

--- pyammanalysis/graphql_helper.py
+++ pyammanalysis/graphql_helper.py
@@ -1,7 +1,10 @@
 import requests
 
-# function to use requests.post to make an API call to the subgraph url
+
 def run_query(url: str, query):
+    """
+    Use ``requests.post`` to make an API call to the subgraph URL
+    """
     # endpoint where you are making the request
     request = requests.post(url, "", json={"query": query})
     if request.status_code == 200:
