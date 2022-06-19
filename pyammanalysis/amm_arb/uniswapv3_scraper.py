b83ff299e21279524b5a70cd3446d63200021a5a
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-06-19 13:34:41 +0100

    adjacency matrix

--- pyammanalysis/amm_arb/uniswapv3_scraper.py
+++ pyammanalysis/amm_arb/uniswapv3_scraper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from pyammanalysis.graphql_helper import run_query
 
@@ -24,6 +25,7 @@ def top_uniswapv3_pairs():
             },
             feeTier,
             liquidity,
+            token0Price,
             token1Price
         }
     }
@@ -55,7 +57,7 @@ def top_uniswapv3_tokens():
     return tokens_list
 
 
-def get_connected_pairs_from_ex():
+def get_connected_tokens():
     """
     Loads the pairs from Uniswap V3 that have 3 or more connections
 
@@ -71,6 +73,32 @@ def get_connected_pairs_from_ex():
     token_count = dict(zip(unique, counts))
 
     return {k: v for k, v in token_count.items() if len(v) >= 3}
+
+
+def create_adj_matrix(outfile="adjacency_matrix.csv"):
+    # fetch list of top token symbols
+    top_100_tokens_dicts = top_uniswapv3_tokens()["tokens"][:100]
+    addr2sym_dict = {token["id"]: token["symbol"] for token in top_100_tokens_dicts}
+    top_100_tokens = list(map(lambda x: x["id"], top_100_tokens_dicts))
+
+    # create empty df
+    df = pd.DataFrame(columns=top_100_tokens, index=top_100_tokens, dtype=np.float64)
+
+    # TODO: consider multi-paths for the same pairs
+    # fill in the adjacency matrix
+    for pool in top_uniswapv3_pairs()["pools"]:
+        token0 = pool["token0"]
+        token1 = pool["token1"]
+        if token0 in top_100_tokens and token1 in top_100_tokens:
+            # indexing allows chaining: df[a][b] * df[b][c] = df[a][c]
+            df[token0][token1] = pool["token1Price"]
+            df[token1][token0] = pool["token0Price"]
+
+    # map addr to symbol in the final step
+    df = df.rename(index=addr2sym_dict).rename(columns=addr2sym_dict)
+
+    df.to_csv(outfile)
+    return df
 
 
 if __name__ == "__main__":
