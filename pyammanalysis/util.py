363505a134145eba13bc749716381859ee515016
StardustLID <60168779+StardustLID@users.noreply.github.com>
2022-06-21 23:05:30 +0100

    flexible plots, config

--- pyammanalysis/util.py
+++ pyammanalysis/util.py
@@ -1,5 +1,12 @@
 from yaml import safe_load
 
+DATA_PATH = "data"
+
+SUBPLOTS_PER_ROW = 3
+SUBPLOT_WIDTH = 8
+SUBPLOT_HEIGHT = 5
+PLOT_WIDTH = SUBPLOTS_PER_ROW * SUBPLOT_WIDTH
+
 
 def read_yaml(file_path):
     with open(file_path, "r") as f:
